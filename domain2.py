a71e6bf731125f1f7b3cb26910f278f348bafda7
cheatercodding <amabazakeremabaza@gmail.com>
2024-10-10 16:06:57 +0300

    1

--- domain2.py
+++ domain2.py
@@ -8,4 +8,10 @@ dosya_yolu = 'domains.csv'
 with open(dosya_yolu, newline='') as csvfile:
     okuyucu = csv.reader(csvfile, delimiter='|')
     for satir in okuyucu:
-        print(f"Domain: {satir[0]}, Başlangıç Tarihi: {satir[1]}, Bitiş Tarihi: {satir[2]}, Durum: {satir[3]}")
+        print(f"Satır: {satir}")  # Satırın tam içeriğini yazdır
+
+       # Satırda 4 sütun olup olmadığını kontrol et
+        if len(satir) == 4:
+            print(f"Domain: {satir[0]}, Başlangıç Tarihi: {satir[1]}, Bitiş Tarihi: {satir[2]}, Durum: {satir[3]}")
+        else:
+            print(f"Geçersiz veya eksik bilgi içeren satır: {satir}")
